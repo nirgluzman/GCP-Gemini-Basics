e69f9255febcbc1f059fd8c4eb58877ac9a84b6c
nirgluzman <nir.gluzman@gmail.com>
2024-07-10 11:50:10 +0200

    create function to generate text from text-only input

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 // The Gemini 1.5 models are versatile and work with most use cases
 const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+
+// Generate text from text-only input, https://ai.google.dev/gemini-api/docs/get-started/tutorial?lang=node
+async function generateText(prompt) {
+  const result = await model.generateContent(prompt);
+  const response = result.response;
+  return response.text();
+}
